f6da237874ce6da15284ccf8612f252ddc9f563a
felipedebiazi <fdebiazi@hotmail.com>
2022-08-09 21:08:47 -0300

    frist commit

---
+++ src/main/java/exemploConexao/ConnectionJDBC.java
@@ -0,0 +1,34 @@
+package exemploConexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionJDBC {
+
+    public static void main(String[] args) throws SQLException {
+
+        // Definir parâmetros para se conectar ao banco de dados MySQL.
+        String driver = "mysql";
+        String dataBaseAddress = "localhost";
+        String dataBaseName = "digital_innovation_one";
+        String user = "root";
+        String password = "password";
+
+        // Construção da string de conexão.
+        StringBuilder sb = new StringBuilder("jdbc:")
+                .append(driver).append("://")
+                .append(dataBaseAddress).append("/")
+                .append(dataBaseName);
+
+        String connectionUrl = sb.toString(); //sb.toString() == "jdbc:mysql://localhost/digital_innovation_one"
+
+        //  Criar conexão usando o DriverManager, passando como parâmetros a string de conexão, usuário e senha do usuário.
+        try (Connection conn = DriverManager.getConnection(connectionUrl, user, password)) {
+            System.out.println("SUCESSO ao se conectar ao banco MySQL!");
+        } catch (SQLException e) {
+            System.out.println("FALHA ao se conectar ao banco MySQL!");
+            e.printStackTrace();
+        }
+    }
+}
